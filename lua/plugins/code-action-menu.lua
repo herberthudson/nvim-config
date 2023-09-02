07cf749afbc864b1a21860f8444fdfc1db50bb83
Herbert Hudson <herberthudson@gmail.com>
2023-09-01 22:34:59 -0300

    add code-action-menu

---
+++ lua/plugins/code-action-menu.lua
@@ -0,0 +1,12 @@
+return {
+  "weilbith/nvim-code-action-menu",
+  cmd = "CodeActionMenu",
+  dependencies = {
+    "kosayoda/nvim-lightbulb",
+  },
+  config = function()
+    require("nvim-lightbulb").setup({
+      autocmd = { enabled = true },
+    })
+  end,
+}
