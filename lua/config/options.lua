d5e8df19a52ba0a10a496e62033adb04e91a43e6
Herbert Hudson <herberthudson@gmail.com>
2023-09-10 00:00:49 -0300

    config fold

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- Treesitter folding
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+
+-- Set fold opening
+vim.wo.foldlevel = 99
