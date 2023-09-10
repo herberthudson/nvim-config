432e6873b66b2439270b598b7cbb082f4ba2bb17
Herbert Hudson <herberthudson@gmail.com>
2023-09-09 23:56:25 -0300

    add catpuuccin

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+    },
+    config = function()
+      require("notify").setup({
+        background_colour = "#000000",
+      })
+    end,
+  },
+  { "LazyVim/LazyVim", opts = {
+    colorscheme = "catppuccin",
+  } },
+}
