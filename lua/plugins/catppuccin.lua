0cdb3e359d94496b1d7c653134ed76795d9f23cd
Herbert Hudson <herberthudson@gmail.com>
2024-03-17 11:14:05 -0300

    fix: update notify

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,17 +5,13 @@ return {
     priority = 1000,
     opts = {
       flavour = "mocha",
-    },
-    config = function()
-      require("notify").setup({
-        background_colour = "#000000",
-      })
-    end,
+    }
   },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
+      background_colour = "#000000",
     }
   },
 }
