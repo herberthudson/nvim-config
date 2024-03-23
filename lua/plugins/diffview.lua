1f5be8d358dfb7d98e8c9f0723856324ff79b2e0
Herbert Hudson <herberthudson@gmail.com>
2024-03-23 19:02:53 -0300

    feat: config diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,4 +1,8 @@
 return {
   "sindrets/diffview.nvim",
   cmd = "DiffviewOpen",
+  keys = {
+    { "<leader>gd", "<cmd>DiffviewOpen<cr>",  desc = "Open Diff View" },
+    { "<leader>go", "<cmd>DiffviewClose<cr>", desc = "Close Diff View" }
+  },
 }
